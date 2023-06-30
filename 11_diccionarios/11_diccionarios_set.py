73b1730403dce1a4398119053c4438e558c2c7bd
diegofer10 <88346788+diegofer10@users.noreply.github.com>
2023-06-30 10:06:50 -0300

    Update 11_diccionarios_set.py

--- 11_diccionarios/11_diccionarios_set.py
+++ 11_diccionarios/11_diccionarios_set.py
@@ -1,10 +1,16 @@
 """
 Diccionarios y Set 
-Set:  tipo de dato pero no tiene indice ni orden
-Diccionario: tipo de datos que tiene formato parecido a json
-"""
+Set:  El tipo set en Python es la clase utilizada por el lenguaje para representar los conjuntos. 
+      Un conjunto es una colección desordenada de elementos únicos, es decir, que no se repiten.
+
+Diccionario:  Un diccionario en Python es una estructura de datos que permite almacenar cualquier tipo de información, desde cadenas de texto o caracteres hasta números enteros, con decimales, listas e incluso otros diccionarios. Al igual que sucede con un diccionario de lengua, los datos se encuentran ordenados utilizando una clave única para cada uno de ellos, lo que permite localizar cada uno de los datos de una forma muy rápida. 
+Los diccionarios Python son mutables, esto quiere decir que no tienen un tamaño predefinido y que su contenido aumenta o disminuye según las necesidades de la aplicación. Todos los datos son también modificables, es decir, se puede añadir, modificar, eliminar y consultar todos los datos de una manera sencilla y rápida.
+Se trata de una estructura de datos similar a las listas, las tuplas o los conjuntos. Se utilizan básicamente para almacenar una gran cantidad de datos de forma ordenada
 
 #Ejericio 1 definir set de usuarios agregar y eliminar datos
+"""
+
+ 
 usuarios =  {
     'diego','juan','pedro','lucia'
     }
